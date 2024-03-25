21a75fba9e7658e4eeddd44c29d6a5421c0326ac
Alexander Samaniego <69481177+alexsam29@users.noreply.github.com>
2024-03-25 00:56:02 -0400

    Add CSVReader class and plot goals per game

--- app/app.py
+++ app/app.py
@@ -1,6 +1,9 @@
+from utils.csv_reader import CSVReader
 from table import Table
 from utils.csv_writer import CSVWriter
 from scraper import Scraper
+import matplotlib.pyplot as plt
+import numpy as np
 
 URL = "https://www.hockey-reference.com/leagues/NHL_2024_skaters.html"
 file_name = "NHL_2024_Stats.csv"
@@ -11,3 +14,17 @@ table = Table(page)
 
 writer = CSVWriter(file_name)
 writer.write(table.getRows())
+
+reader = CSVReader(file_name)
+csv_stats = reader.read()
+
+# Plotting the data
+for i in range(len(csv_stats)):
+    plt.scatter(csv_stats['GP'][i], csv_stats['G'][i])  # Plot each point
+    plt.text(csv_stats['GP'][i], csv_stats['G'][i], csv_stats['Player'][i])  # Label each point
+    
+plt.title('Goals per Game')
+plt.xlabel('Games Played')
+plt.ylabel('Goals')
+plt.savefig("GPG.png")  # Save the plot as a PNG file
+plt.show()
